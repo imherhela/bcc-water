9ff7a23d314a42dc6bdc68e2f044a8cf43afb1ed
ImHerHela <76200467+imherhela@users.noreply.github.com>
2025-03-26 03:05:52 -0700

    Update water.sql

--- water.sql
+++ water.sql
@@ -3,8 +3,11 @@ VALUES
     ('canteen', 'Canteen', 1, 1, 'item_standard', 1, 'A portable container to carry water.'),
     ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1, 'A bucket of water.'),
     ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1, 'An empty water bucket.'),
+    ('wateringcan_dirtywater', 'Dirty Water Jug', 10, 1, 'item_standard', 1, 'A bucket filled with dirty water.'),
     ('bcc_empty_bottle', 'Empty Bottle', 15, 1, 'item_standard', 1, 'An empty bottle.'),
-    ('bcc_full_bottle', 'Water Bottle', 15, 1, 'item_standard', 1, 'A bottle of water.')
+    ('bcc_clean_bottle', 'Clean Water Bottle', 15, 1, 'item_standard', 1, 'A bottle filled with clean water.'),
+    ('bcc_dirty_bottle', 'Dirty Water Bottle', 15, 1, 'item_standard', 1, 'A bottle filled with dirty water.'),
+    ('antidote', 'Antidote', 5, 1, 'item_standard', 1, 'A remedy to cure sickness.')
 ON DUPLICATE KEY UPDATE
     `item` = VALUES(`item`),
     `label` = VALUES(`label`),
